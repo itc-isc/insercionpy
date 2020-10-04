1a95dbc3bb98a634c17d83afcfc54d902cb52498
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2020-10-04 17:54:54 +0000

    Quitando ; en algunas instrucciones...

--- insercion.py
+++ insercion.py
@@ -3,14 +3,14 @@ def insercion(A):
     for i in range(len(A)):
         for j in range(i,0,-1):
             if(A[j-1] > A[j]):
-                aux=A[j];
-                A[j]=A[j-1];
-                A[j-1]=aux;
+                aux=A[j]
+                A[j]=A[j-1]
+                A[j-1]=aux
 
 #Programa Principal
 
-A=[6,5,3,1,8,7,2,4];
-print(A);
+A=[6,5,3,1,8,7,2,4]
+print(A)
 
-insercion(A);
+insercion(A)
 print(A)
